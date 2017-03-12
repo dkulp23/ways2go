340bb5024d41bcb918af6691ea7ec0a48fa4fd28
Dana Kulp <dkulp23@gmail.com>
2017-03-12 14:54:17 -0700

    Initial project repository scaffold commit. Basic directory structure in place with placeholder files included.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', function() {
+  return gulp.src(['**/*.js', '!node_modules'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('dev', function() {
+  gulp.watch(['**/*.js', '!node_modules'], ['lint']);
+});
+
+gulp.task('default', ['lint']);
