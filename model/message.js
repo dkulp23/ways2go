fbfb2184166639eb848a2042a3b21fa7d0062527
darms <darmcca@gmail.com>
2017-03-13 13:43:12 -0700

    basic message model

--- model/message.js
+++ model/message.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = Schema({
-  to_user:{ type: String, required: true},
-  from_user: { type: String, required: true},
+  username: { type: String, required: true},
+  to_user_id:{ type: String, required: true},
+  from_user_id: { type: String, required: true},
+  timestamp: { type: Date, required: true},
   text : { type: String, required: true},
-  timestamp: { type: Date, required: true}
 });
 
 module.exports.model('message', messageSchema);
